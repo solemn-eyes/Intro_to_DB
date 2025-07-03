fc83c9b9d051647852aef08047608c7f73423df4
Jerome Maunga <jerhomemaunga@gmail.com>
2025-07-03 12:45:31 +0300

    Another trial change

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO Customers ("customer_id", "customer_name", "email", "address") VALUES (
+INSERT INTO customer ("customer_id", "customer_name", "email", "address") VALUES (
     1,
     'Cole Baidoo',
     'cbaidoo@sandtech.com',
